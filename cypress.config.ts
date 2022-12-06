8a5594c273f4766f256d5d663b773cd11c28e859
Mobin Nikraftar <mobinnikraftar@gmail.com>
2022-12-06 22:29:00 +0300

    fix Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
 
+	env: {
+		// This is the project ID from the Cypress Dashboard
+		// https://dashboard.cypress.io/projects/4g6oks/settings
+		// It is used to record test results
+		CYPRESS_RECORD_KEY: '9b672414-e3bc-410a-9af6-6e1a2853f5a5',
+		projectId: '4g6oks',
+	},
 	e2e: {
-		env: {
-			// This is the project ID from the Cypress Dashboard
-			// https://dashboard.cypress.io/projects/4g6oks/settings
-			// It is used to record test results
-			CYPRESS_RECORD_KEY: '9b672414-e3bc-410a-9af6-6e1a2853f5a5',
-			projectId: '4g6oks',
-		},
 		setupNodeEvents: (on, config) => {
 			const isDev = config.watchForFileChanges
 			const port = process.env.PORT ?? (isDev ? '3000' : '8811')
